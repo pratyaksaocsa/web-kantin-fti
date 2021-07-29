c58e97d6b94a7de2eb5477603d05d036845985dc
Yulius <yiyus49@gmail.com>
2021-07-29 21:59:51 +0700

    Responsive Web Design

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/slick/slick.css">
-  <link rel="stylesheet" href="assets/slick/slick-theme.css">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/slick/slick.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/slick/slick-theme.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
   <title>Order Kantin FTI</title>
 </head>
 <body>
@@ -36,9 +36,9 @@
       <div class="layer-background-order">
         <div class="d-flex h-100 flex-column align-items-center justify-content-center">
           <div class="banner-icon-img">
-            <img src="/assets/img/uksw.png" alt="UKSW" class="icon-img">
-            <img src="/assets/img/fti.png" alt="FTI" class="icon-img">
-            <img src="/assets/img/lk.png" alt="LK" class="icon-img">
+            <img src="{{ asset('assets/img/uksw.png') }}" alt="UKSW" class="icon-img">
+            <img src="{{ asset('assets/img/fti.png') }}" alt="FTI" class="icon-img">
+            <img src="{{ asset('assets/img/lk.png') }}" alt="LK" class="icon-img">
           </div>
           <div class="banner-title">
             <h2 class="banner-title-text">Pemesanan</h2>
@@ -50,12 +50,12 @@
   <div class="container ">
       <div class="bg-white order shadow">
     <!-- Order top -->
-      <div class="order-top mt-2 d-flex justify-content-between">
-        <div class="col-md-4 col-lg-4">
-            <img src="/assets/img/resep-kue-lemper-ayam.jpg" alt="Lemper" class="order-img">
+      <div class="order-top mt-2 row">
+        <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12 float-start">
+            <img src="{{ asset('assets/img/resep-kue-lemper-ayam.jpg') }}" alt="Lemper" class="order-img">
         </div>
-        <div class="col-md-7 col-lg-7 order-detail">
-            <div class="order-detail-top">
+        <div class="col-md-7 col-lg-7 order-detail float-end">
+            <div class="order-detail-top mb-3">
                 <div class="order-detail-title">
                     <h2 class="order-detail-title-text">Lemper</h2>
                 </div>
@@ -75,36 +75,35 @@
       </div>
     <!-- Order middle -->
     <form>
-        <div class="order-middle mt-5 d-flex justify-content-between">
-            <div class="col-md-4 col-lg-4 ">
-                <h4> <strong> Jumlah</strong></h4>
+        <div class="order-middle row align-items-center">
+          <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 float-start ">
+              <h4> <strong> Jumlah</strong></h4>
+          </div>
+          <div class="col-md-7 col-lg-7 col-sm-12 col-xs-12  float-end">
+            <div class="input-group jumlah">
+              <button type="button" onclick="decrement()"  class="btn btn-primary" > - </button>
+              <input type="number" step="1" max="100" value="1" name="jumlah" class="form-control" id="jumlah" />
+              <button type="button" onclick="increment()"  class="btn btn-primary"> + </button>
             </div>
-            <div class="col-md-7 col-lg-7">
-                
-                  <div class="input-group jumlah">
-                    <button type="button" onclick="decrement()"  class="btn btn-primary" > - </button>
-                    <input type="number" step="1" max="100" value="1" name="jumlah" class="form-control form-control-sm" id="jumlah" />
-                    <button type="button" onclick="increment()"  class="btn btn-primary"> + </button>
-                  </div>
-            </div>
+          </div>
         </div>
-        <button type="submit" class="btn btn-primary w-100 shadow mt-2"> <h5>Pesan </h5></button>
+        <button type="submit" class="btn btn-primary w-100 shadow mt-2"> <h5 class="btn-txt">Pesan </h5></button>
     </form>
     </div>
   </div>
   <footer class="bg-primary text-white text-center text-lg-start">
   <div class="container p-4">
-    <div class="row align-items-center">
-      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">kantin fti</h5>
+    <div class="row align-items-center ">
+      <div class="col-lg-4 col-md-4 mb-4 mb-md-0 text-left">
+        <h5 class="text-uppercase left">kantin fti</h5>
       </div>
-      <div class="col-lg-4 col-md-6 mb-4 mb-md-0 d-flex justify-content-around">
-        <img src="/assets/img/uksw.png" alt="UKSW" class="icon-footer">
-        <img src="/assets/img/fti.png" alt="FTI" class="icon-footer">
-        <img src="/assets/img/lk.png" alt="LK" class="icon-footer">
+      <div class="col-lg-4 col-md-4 mb-4 mb-md-0 d-flex justify-content-around ">
+        <img src="{{ asset('assets/img/uksw.png') }}" alt="UKSW" class="icon-footer">
+        <img src="{{ asset('assets/img/fti.png') }}" alt="FTI" class="icon-footer">
+        <img src="{{ asset('assets/img/lk.png') }}" alt="LK" class="icon-footer">
       </div>
-      <div class="col-lg-4 col-md-6 mb-4 mb-md-0 ">
-        <ul class="list-unstyled mb-0 float-end">
+      <div class="col-lg-4 col-md-4 mb-4 mb-md-0 list-menu">
+        <ul class="list-unstyled mb-0 float-lg-end float-md-end float-sm-center float-xs-center">
           <li>
             <a href="#" class="footer-link">Menu 1</a>
           </li>
@@ -125,11 +124,11 @@
     </div>
   </div>
 </footer>
-  <script src="assets/js/script.js"></script>
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/bootstrap.js"></script>
+  <script src="{{ asset('assets/js/script.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
     
-  <script src="assets/slick/slick.min.js"></script>
-  <script src="assets/js/script.js"></script>
+  <script src="{{ asset('assets/slick/slick.min.js') }}"></script>
+  <script src="{{ asset('assets/js/script.js') }}"></script>
 </body>
 </html>
