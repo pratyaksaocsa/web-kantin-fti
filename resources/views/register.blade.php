36debc9312cae0fe1c3031dafa321635f312d049
Sinatria Banyu Adil <87744334+svnflxwer@users.noreply.github.com>
2021-08-03 21:50:55 +0700

    Add files via upload

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,51 +7,64 @@
   <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/slick/slick.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/slick/slick-theme.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/register.css') }}">
   <title>Kantin FTI | Registration Page</title>
 </head>
-<body class="d-flex h-100 text-dark text-justify bg-primary">
+<body class="form-v6">
   <!-- Menu -->
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto p-auto flex-column">
-        <div class="container p-3 my-auto bg-light rounded">
-          <div class="row col-lg-12 col-md-12">
+  
+	<div class="page-content">
+		<div class="form-v6-content">
+			<div class="form-left">  
+      <div class="row col-lg-12 col-md-12">
             <div class="col-md-6 col-lg-6 ">
               <img src="/assets/img/makananregist.jpg" class="w-100"> 
             </div>
             
               
             <div class="col-md-6 col-lg-6 ">
-              <h1>Register</h1>
-              <form action="">
-              <div class="form-group">
-                <label class="form-label">Nama	</label>
-                <input type="text" name="Nama" placeholder="(Your Name)" class="form-control">
+            <form class="form-detail" action="#" method="post">
+              <h2><strong>Registrasi Kantin UKSW</strong></h2>
+              <div class="form-row">
+                <input type="text" name="Nama" placeholder="Your Name" class="input-text">
               </div>
-				      <div class="form-group">
-                <label class="form-label">Username	</label>
-				    	  <input type="text" name="Username" class="form-control" placeholder="(Username)" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
+				      <div class="form-row">
+				    	  <input type="text" name="Username" class="input-text" placeholder="Username" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
 				      </div>
-              <div class="form-group">
-                <label class="form-label">Email/Number	</label>
-				    	  <input type="text" name="Email/Number" class="form-control" placeholder="(Email or Number)" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
+              <div class="form-row">
+				    	  <input type="text" name="Email/Number" class="input-text" placeholder="Email or Number" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
 				      </div>
-              <div class="form-group">
-                <label class="form-label">ID	</label>
-					      <input type="number" name="id" class="form-control" placeholder="(ID)" required>
+              <div class="form-row">
+					      <input type="number" name="id" class="input-text" placeholder="ID" required>
 			      	</div>
-              <div class="form-group">
-                <label class="form-label">Password	</label>
-				        <input type="password" name="Password" placeholder="(Password)"class="form-control">
+              <div class="form-row">
+				        <input type="password" name="Password" placeholder="Password"class="input-text">
               </div>
-              <div class="form-group">
-                <button class="btn btn-primary w-100">Register</button>
-              </div>
+              <div class="form-row-last">
+				    	  <input type="submit" name="register" class="register" value="Register">
+			      	</div>   
+              <center>
+              <h3>
+                <font size="3" color="black">Sudah punya akun? </font>
+                <a href="#" title="Menuju halaman Login"><font size="3">Silahkan Login</font></a>
+              </h3>
+              </center>
               </form>
             </div>
-            
 
           </div>
-
+          <div class="form-bottom">
+          <form action="#" method="get">
+              <center>
+                <h3>space<input type='checkbox' name='kebijakanprivasi' value='kebijakanprivasi' />
+                <font size="3" color="black">Saya telah membaca dan menyetujui </font>
+                <a href="#" title="Menuju halaman Penggunaan"><font size="3">Penggunaan</font></a>
+                <font size="3" color="black" margin-bottom:-2px> dan </font>
+                <a href="#"  title="Menuju halaman Kebijakan Privasi"><font size="3">Kebijakan Privasi</font></a>
+                </h3>
+              </center>
+            </form>
+          </div>
         </div>
     </div>
 </footer>
